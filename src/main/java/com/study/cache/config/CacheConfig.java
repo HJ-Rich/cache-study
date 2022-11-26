0cffe3c1ea8a8d8f6fcb5ede3a4cc04d0352ede1
Richard JEON <ztzy1907@gmail.com>
2022-11-27 06:44:32 +0900

    feat: cache 설정 활성화

---
+++ src/main/java/com/study/cache/config/CacheConfig.java
@@ -0,0 +1,9 @@
+package com.study.cache.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@EnableCaching
+@Configuration
+public class CacheConfig {
+}
