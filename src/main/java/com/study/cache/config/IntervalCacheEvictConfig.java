14e7332ccb357ff6a86b6d0cff56f7cb52e530c7
Richard JEON <ztzy1907@gmail.com>
2022-11-27 06:54:08 +0900

    feat: 애너테이션 기반, 캐시 매니저 기반 evict 설정 추가

--- src/main/java/com/study/cache/config/IntervalCacheEvictConfig.java
+++ src/main/java/com/study/cache/config/IntervalCacheEvictConfig.java
@@ -2,6 +2,8 @@ package com.study.cache.config;
 
 import static com.study.cache.application.JpaTeamService.ALL_TEAMS_CACHE_KEY;
 
+import java.util.Objects;
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -10,9 +12,22 @@ import org.springframework.scheduling.annotation.Scheduled;
 @EnableScheduling
 @Configuration
 public class IntervalCacheEvictConfig {
+    private final CacheManager cacheManager;
+
+    public IntervalCacheEvictConfig(final CacheManager cacheManager) {
+        this.cacheManager = cacheManager;
+    }
+
     @CacheEvict(cacheNames = "teams", key = ALL_TEAMS_CACHE_KEY)
-    @Scheduled(fixedDelay = 10000)
-    public void evict() {
-        System.out.println("evict cache...");
+    @Scheduled(fixedDelay = 5000)
+    public void evictByAnnotation() {
+    }
+
+    @Scheduled(fixedDelay = 7000)
+    public void evictByCacheManager() {
+        final var teams = cacheManager.getCache("teams");
+        if (Objects.nonNull(teams)) {
+            teams.clear();
+        }
     }
 }
